ebbeef349ce13012a3a60b421e9a4fccf4fc5548
tkdtn4657 <tkdtn101226@gmail.com>
2023-04-05 16:07:09 +0900

    init

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -0,0 +1,81 @@
+package com.ll.gramgram.boundedContext.likeablePerson.controller;
+
+import com.ll.gramgram.base.rq.Rq;
+import com.ll.gramgram.base.rsData.RsData;
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.service.DataNotFoundException;
+import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/likeablePerson")
+@RequiredArgsConstructor
+public class LikeablePersonController {
+    private final Rq rq;
+    private final LikeablePersonService likeablePersonService;
+
+    @GetMapping("/add")
+    public String showAdd() {
+        return "usr/likeablePerson/add";
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class AddForm {
+        private final String username;
+        private final int attractiveTypeCode;
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid AddForm addForm) {
+        RsData<LikeablePerson> createRsData = likeablePersonService.like(rq.getMember(), addForm.getUsername(), addForm.getAttractiveTypeCode());
+
+        if (createRsData.isFail()) {
+            return rq.historyBack(createRsData);
+        }
+
+        return rq.redirectWithMsg("/likeablePerson/list", createRsData);
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        InstaMember instaMember = rq.getMember().getInstaMember();
+
+        // 인스타인증을 했는지 체크
+        if (instaMember != null) {
+            List<LikeablePerson> likeablePeople = likeablePersonService.findByFromInstaMemberId(instaMember.getId());
+            model.addAttribute("likeablePeople", likeablePeople);
+        }
+
+        return "usr/likeablePerson/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String likeablePersonDelete(Principal principal, @PathVariable("id") Integer id) throws DataNotFoundException {
+        LikeablePerson likeablePerson = likeablePersonService.getPerson(id);
+//        if (!likeablePerson.getToInstaMember().equals(principal.getName())){
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+//        }
+        likeablePersonService.delete(likeablePerson);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/DataNotFoundException.java
@@ -0,0 +1,6 @@
+package com.ll.gramgram.boundedContext.likeablePerson.service;
+
+public class DataNotFoundException extends Throwable {
+    public DataNotFoundException(String questionNotFound) {
+    }
+}
